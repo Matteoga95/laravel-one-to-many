cb1698400bcf0292b021407e9bce47b622399831
matteoga95 <gallo.work95@gmail.com>
2023-01-16 16:10:40 +0100

    metodi per definire relazione nei model aggiunti

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
 
     protected $fillable = ['project_title', 'body', 'slug'];
+
+
+    /**
+     * @return \illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
